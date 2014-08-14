5e011510617c07c1a664e8ef04c0e2d6608b1a83
Rabeeshkumar A R <rabish.kumar@bridge-india.in>
2014-08-14 12:57:15 +0530

    save user info into database

--- server.go
+++ server.go
@@ -5,8 +5,8 @@ import (
     "net/http"
     "io"
     "encoding/json"
-    // "gopkg.in/mgo.v2"
-    // "gopkg.in/mgo.v2/bson"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
 type User struct {
@@ -14,18 +14,47 @@ type User struct {
     Email string `json:"email"`
 }
 
+func saveUserinfo( u *User) User{
+    session, err := mgo.Dial("mongodb://localhost:27017")
+    if err != nil {
+        panic(err)
+    }
+    defer session.Close()
+
+    fmt.Println("saving data into mongo");
+    // Optional. Switch the session to a monotonic behavior.
+    session.SetMode(mgo.Monotonic, true)
+
+    c := session.DB("goquiz").C("users")
+    err = c.Insert(&u)
+    if err != nil {
+        fmt.Println("error saving data into mongo");    
+        panic(err)
+    }
+
+    result := User{}
+    err = c.Find(bson.M{"name": u.Name }).One(&result)
+    if err != nil {
+        panic(err)
+    }
+
+    return result
+}
 func userinfo( res http.ResponseWriter, req *http.Request ){
 
     name := req.FormValue("name" )
     email := req.FormValue("email" )
 
     u := &User{name, email}
+
+    saveUserinfo(u);
+    encoded, err := json.Marshal( u )
+
     jsonString := ""
-    encoded, err := json.Marshal(u)
-    if err != nil{
-        jsonString = "{\"error\": \"error\"}"
-    } else {
+    if err == nil{
         jsonString = string( encoded )
+    } else {
+        jsonString = "{\"error\": \"error\"}"
     }
     res.Header().Set(
         "Content-Type",
