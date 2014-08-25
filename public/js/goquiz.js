69af9116ef24d3292fab3044f08aec5cf6e41708
Rabeeshkumar A R <rabish.kumar@bridge-india.in>
2014-08-25 18:13:52 +0530

    listing questions

--- public/js/goquiz.js
+++ public/js/goquiz.js
@@ -1,17 +1,41 @@
-
-angular.module('goquizApp',[])
-    .controller('userCtrl', ['$scope', '$http', function( $scope , $http ){
+angular.module('goquizApp', [])
+    .controller('userCtrl', ['$scope', '$http', 'quizService', function( $scope , $http , quizService  ){
         $scope.data = {
             'name' :'',
             'email' :'',
+            'show' : true
         };
+        
         $scope.take = function(){
-            $http.post('/userinfo', $scope.data )
-            .success(function(data) {
-                console.log( data )
-            })
-            .error(function(data) {
-                console.log('error in saving user');
-            });
+            $scope.data.show = false;
+            quizService.post( $scope.data )
         }
+    }])
+    .controller('quesCtrl', ['$scope', '$http', 'quizService', function( $scope , $http , quizService ){
+        var quiz = {
+            questions : []
+        };
+        $scope.quiz = quiz  ;
+        $scope.data ={
+            show : false
+        };
+        quizService.set( $scope );
+    }])
+    .factory('quizService', [ '$http', function($http){
+        var $scope ;
+        return {
+            set : function ( scope ){
+                $scope = scope ;
+            },
+            post : function ( data ){
+                $http.post('/userinfo', data )
+                .success(function(data) {
+                    $scope.quiz = data;
+                    $scope.data.show = true  ;
+                })
+                .error(function(data) {
+                    console.log('error in saving user');
+                });
+            },
+        };
     }]);
